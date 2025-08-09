275b684c16fabfc2ad59eac2563ab361e6060712
Daniel <dsebastian.villa@gmail.com>
2025-08-09 10:50:20 -0500

    Algun cambio que no tengo ni idea de que hice

--- Assets/Scripts/Player/AnimationController.cs
+++ Assets/Scripts/Player/AnimationController.cs
@@ -2,4 +2,20 @@ using UnityEngine;
 
 public class AnimationController : MonoBehaviour
 {
+    [SerializeField] GroundDetector groundDetector;
+    Animator animator;
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        animator.SetBool("OnGround", groundDetector.DetectGround());
+        animator.SetFloat("RbYVelocity", rb.linearVelocityY);
+        animator.SetBool("OnMovement", rb.linearVelocityX != 0);
+    }
 }
