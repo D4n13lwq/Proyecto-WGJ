e826e840649bed04077974c86d5a4698b14fc3c0
XxZerouwuxX <dsebastian.villa@gmail.com>
2025-08-09 01:38:43 -0500

    Boton para usar linterna

---
+++ Assets/Scripts/Player/Flashlight.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Flashlight : MonoBehaviour
+{
+    [SerializeField] GameObject flashlightObject;
+    PlayerControls playerControls;
+
+    private void Awake()
+    {
+        playerControls = TryGetComponent(out playerControls) ? playerControls : gameObject.AddComponent<PlayerControls>();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.InputSystem.Player.Attack.performed += TurnOnFlashlight;
+        playerControls.InputSystem.Player.Attack.canceled += TurnOffFlashlight;
+    }
+
+    private void OnDisable()
+    {
+        playerControls.InputSystem.Player.Attack.performed -= TurnOnFlashlight;
+        playerControls.InputSystem.Player.Attack.canceled -= TurnOffFlashlight;
+    }
+
+    void TurnOnFlashlight(InputAction.CallbackContext context) => flashlightObject.SetActive(true);
+    void TurnOffFlashlight(InputAction.CallbackContext context) => flashlightObject.SetActive(false);
+}
