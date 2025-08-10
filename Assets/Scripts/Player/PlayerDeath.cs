b3d6fb738ed7bb90e390471b342a4954ffa2af7b
Daniel <dsebastian.villa@gmail.com>
2025-08-10 01:21:21 -0500

    Un commit mas sin poronga idea(ya perdi la cuenta)

--- Assets/Scripts/Player/PlayerDeath.cs
+++ Assets/Scripts/Player/PlayerDeath.cs
@@ -3,18 +3,6 @@ using UnityEngine;
 
 public class PlayerDeath : MonoBehaviour
 {
-<<<<<<< Updated upstream
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-=======
     private Animator anim;
 
     private void Start()
@@ -25,6 +13,5 @@ public class PlayerDeath : MonoBehaviour
     public void Death()
     {
         anim.SetTrigger("Die");
->>>>>>> Stashed changes
     }
 }
